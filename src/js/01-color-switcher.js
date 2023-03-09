ce9d9dc972b930abf9a915c3d0eaabdf364587d5
Олена Пестова <nowalena9@gmail.com>
2023-03-09 11:56:35 +0200

    color-switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  
+}
+//посилання 
+const refs = {
+    body: document.body,
+    btnStart: document.querySelector("button[data-start]"),
+    btnStop: document.querySelector("button[data-stop]"),
+}
+//слухачі подій
+refs.btnStart.addEventListener('click', start);
+refs.btnStop.addEventListener('click', stop);
+
+let idTimer = null;//оголошуємо idTimer в глобальній зоні видимості
+
+function start() {
+    refs.btnStart.disabled = true; //після запуска кнопка start недоступна //або refs.btnStart.removeEventListener('click', start);
+    setColor(); //щоб перша зміна відбулась відразу
+   idTimer = setInterval(setColor, 1000);//заплановано що 1с запускати setColor до бескінечності
+};
+function stop() {
+    clearInterval(idTimer); //по id таймера setInterval відміняємо що 1с запускати setColor
+    refs.btnStart.disabled = false; //кнопка start знову доступна //або refs.btnStart.addEventListener('click', start);
+};
+//зміна кольору body 
+function setColor() {
+   let bgColor= getRandomHexColor(); //генеруємо колір
+    refs.body.style.backgroundColor = bgColor; //змінюємо колір body
+};
+
 

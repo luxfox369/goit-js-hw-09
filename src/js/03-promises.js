4708502ef0e77290641ffe762883f3caf6fb4a43
Олена Пестова <nowalena9@gmail.com>
2023-03-15 18:52:07 +0200

    Promise.all/race/any/allSettled

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,5 @@
 import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
@@ -23,16 +24,31 @@ function onSubmit(event) {
   const { elements: {delay, step, amount} } = event.currentTarget;
  
   let countedDelay = Number(delay.value);
-  
+  let arrayPromises = [];
   for (let i = 1; i <= Number(amount.value); i += 1) {
     if (i > 1) { countedDelay += Number(step.value); }
     Notiflix.Loading.standard('Creating promises...');
     const promise = createPromise(i, countedDelay);
     promise
-      .then(value => { Notiflix.Notify.success(value); })
-      .catch(error => { Notiflix.Notify.failure(error);})
+      .then(value => { Notiflix.Notify.success(value) })
+      .catch(error => { Notiflix.Notify.failure(error); })
+    arrayPromises.push(promise);
   }
+  
   Notiflix.Loading.remove(3000);
+
+  const promiseAll = Promise.all(arrayPromises)    //викликається на класі,на екземплярах нема бо метод статичний
+  .then(value => Notiflix.Notify.success(value)) //якщо всі resolve ,то поверне масив рядків resolves []
+  .catch(error => Notiflix.Notify.failure(error));// поверне 1й reject
+  const promiseRace =Promise.race(arrayPromises)    //викликається на класі бо метод статичний
+  .then(value => Notiflix.Notify.success(value)) //поверне результат resolve найскорше  виконаного промісу
+  .catch(error => Notiflix.Notify.failure(error));
+const promiseAny = Promise.any(arrayPromises)    //викликається на класі бо метод статичний
+  .then(value => Notiflix.Notify.success(value)) //поверне результат resolve  найскорше  виконаного resolve промісу 
+  .catch(error => Notiflix.Notify.failure(error));// поверне  результат reject або  aggregateEror якщо всі rejected  
+const promiseAllSettled = Promise.allSettled(arrayPromises)    //викликається на класі бо метод статичний
+  .then(value => Notiflix.Notify.success(value)) //поверне результат масив всіх промісів як resolve/reject коли всі проміси завершені(settled)
+    .catch(error => Notiflix.Notify.failure(error)); 
   event.currentTarget.reset();
 }
 

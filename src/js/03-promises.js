2ab41c83e54f757640bdf663960708a821f46993
Olena Pestova <nowalena9@gmail.com>
2024-05-09 00:20:38 +0300

    створення масива промісів і всі функції над масивом промісів

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,50 +1,48 @@
 import Notiflix from "notiflix";
-
+const arrPromises = [];
+let markUp = '<h3>Масив промісів:</h3>';
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
+    const shouldResolve = Math.random() > 0.5;
     setTimeout(() => {
       if (shouldResolve) {
-        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      } else {
+        resolve( `✅ Fulfilled promise ${position} in ${delay}ms` );
+             } else {
         reject(`❌ Rejected promise ${position} in ${delay}ms`);
       }
     }, delay);
   });
 }
-let arrPromises = [];
+
 const refs = {
   form: document.querySelector(".form"),
   input: document.querySelector("input"),
-  submit:document.querySelector(".submit"),
+  submit: document.querySelector(".submit"),
   buttons: document.querySelector(".buttons"),
   all: document.querySelector(".all"),
   race: document.querySelector(".race"),
   any: document.querySelector(".any"),
   allSet: document.querySelector(".allSettled"),
-  
+  promises: document.querySelector(".promises")
 }
-refs.all.disabled = true;
-refs.race.disabled = true;
-refs.any.disabled = true;
-refs.allSet.disabled = true;
-
-//refs.input.style.cssText = 'display:inline-block;width:50px';
-refs.form.style.cssText = 'display:flex;flex-direction:column;gap:20px;padding:10px';
-refs.submit.style.cssText = 'width:150px;height:50px;padding:10px';
-refs.buttons.style.cssText = 'display:flex;gap:10px;height:50px;font-size:50px;padding:10px';
-
+ // кнопки функцій недоступні
+disabledButtons();
+//стилі
+refs.form.style.cssText = 'display:flex;flex-direction:column;align-items:center;gap:10px;padding:5px';
+refs.submit.style.cssText = 'width:150px;height:50px;padding:5px';
+refs.buttons.style.cssText = 'display:flex;gap:10px;justify-content:center;height:50px;font-size:30px;padding:5px';
+//прослуховувач кнопки
 refs.form.addEventListener("submit", onSubmit);
-
+//створення промісів
+;
 function onSubmit(e) {
   e.preventDefault();
   //ловимо всі input форми  по name 
   const { elements: { delay, step, amount } } = e.currentTarget;
   //вони символьні тому перетворюємо на числа
   let nDelay = Number(delay.value);
-  let nStep = Number(step.value);
-  let nAmount = Number(amount.value);
-  //console.log(nDelay,"/", nStep,"/", nAmount);
+  const nStep = Number(step.value);
+  const nAmount = Number(amount.value);
   
   for (i = 1; i <= nAmount; i += 1) {
     Notiflix.Loading.standard('Creating promises...');
@@ -52,37 +50,61 @@ function onSubmit(e) {
       nDelay += nStep;
     }
     let promise = createPromise(i, nDelay);
+   
     promise
-      .then(data => { Notiflix.Notify.success(data) })
-      .catch(error => { Notiflix.Notify.failure(error) });
-
+      .then(data => {Notiflix.Notify.success(data);markUp   += `<p> ${data}</p>` }) // 
+      .catch(error => {Notiflix.Notify.failure(error);markUp += `<p> ${error}</p>` }) //
+      
+    //формуємо масив отриманих промісів для подальших функцій
     arrPromises.push(promise);
+    //намалюємо отримані проміси на сторінці через markUp
+  
   }
-  Notiflix.Loading.remove(3000);
+  setTimeout(() => {
+    console.log(markUp);
+    refs.promises.innerHTML = markUp;
+    Notiflix.Loading.remove(3000); //забираємо колесо через 3с
+    enabledButtons();
+   
+  }, nDelay+nStep*nAmount);
+  
+   
+  e.currentTarget.reset();
+}
+ 
+  //вішаємо функції на кнопки
   refs.all.addEventListener("click", allPromise);
   refs.race.addEventListener("click", racePromise);
   refs.any.addEventListener("click", anyPromise);
   refs.allSet.addEventListener("click", allSet);
   
-//e.currentTarget.reset();
+  
+ //кнопки функцій недоступні
+ function disabledButtons() {
+  refs.all.disabled = true;
+  refs.race.disabled = true;
+  refs.any.disabled = true;
+  refs.allSet.disabled = true;
 }
-  //кнопки функцій доступні
+
+
+// кнопки функцій доступні
+function enabledButtons() {
   refs.all.disabled = false;
   refs.race.disabled = false;
   refs.any.disabled = false;
   refs.allSet.disabled = false;
-
+}
+//функції над масивом промісів
 function allPromise (){
   const promiseAll = Promise.all(arrPromises)
     .then(value => Notiflix.Notify.success(value)) // when all of the promises fulfill;  
     .catch(error => Notiflix.Notify.failure(error));// rejects when any of the promises rejects and return the first rejected
 }
-//Notiflix.Notify.success(value)
-  //Notiflix.Notify.failure(error)
-  function racePromise() {
+function racePromise() {
     const promiseRace = Promise.race(arrPromises)
       .then(value => Notiflix.Notify.success(value)) //RACE Settles = fulfills when first of the promises fulfills 
-      .catch(error => Notiflix.Notify.failure(error));//rejects when any of the promises rejects.
+      .catch(error => Notiflix.Notify.failure(error));//rejects when first of the promises rejects.
 }
 function anyPromise() {
   const promiseAny = Promise.any(arrPromises)
@@ -95,11 +117,13 @@ function allSet() {
     .catch(error => Notiflix.Notify.failure(error));
 }
 
-//refs.all.removeEventListener("click", allPromise);
-//refs.race.removeEventListener("click", racePromise);
-//refs.any.removeEventListener("click", anyPromise);
-//refs.allSet.removeEventListener("click", allSet);
 
+function clearListener() {
+  refs.all.removeEventListener("click", allPromise);
+  refs.race.removeEventListener("click", racePromise);
+  refs.any.removeEventListener("click", anyPromise);
+  refs.allSet.removeEventListener("click", allSet);
+}
 
 
  
